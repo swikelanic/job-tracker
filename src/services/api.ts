9f9e32e84363489c7613c986c028ed62818ed4f3
Chris Junior Swikelani <swikelanic@gmail.com>
2025-08-09 22:47:48 +0200

    Use env variable for API base URL in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // Change to your deployed URL if needed
+  baseURL: process.env.REACT_APP_API_BASE_URL, // Use env variable for base URL
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default API;
